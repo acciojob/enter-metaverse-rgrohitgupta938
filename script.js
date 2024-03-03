af2c78fc5745c68848237b83f7f5491e652e011d
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-03-03 15:41:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const ptag = document.getElementById("status");
+const btn = document.getElementById("enterBtn");
+
+btn.addEventListener("click",() => {
+	ptag.innerHTML = "<h1>Entered Metaverse</h1>";
+})
